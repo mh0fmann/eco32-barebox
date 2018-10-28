dbb3ad924d422aa69bbf5983ff1f0b30f6ea5d15
Martin Hofmann <martin.hofmann@mni.thm.de>
2018-10-28 12:16:47 +0100

    remove unused makros

--- arch/eco32/boards/generic/config.h
+++ arch/eco32/boards/generic/config.h
@@ -8,14 +8,4 @@
 #define ECO32_SOPC_MEMORY_BASE      0xC0000000
 #define ECO32_SOPC_MEMORY_SIZE      0x02000000
 
-/* We reserve 1M for barebox */
-#define BAREBOX_RESERVED_SIZE       0x00100000
-
-/* Devices */
-#define ECO32_SOPC_UART_BASE        0xF0300000
-#define ECO32_SOPC_DISK_BASE        0xF0400000
-
-/* Where Barebox will be in the memory */
-#define ECO32_SOPC_TEXT_BASE        CONFIG_ARCH_TEXT_BASE
-
 #endif
